95f8b60ddd805a62590676453f5dfe5ebbc28db8
Jørn Åne <git@jornane.no>
2017-10-24 00:38:47 +0200

    Move code to functions

--- ncc.c
+++ ncc.c
@@ -1,14 +1,62 @@
-/*
-	C ECHO client example using sockets
-*/
-#include<stdio.h>	//printf
-#include<sys/socket.h>	//socket
-#include<arpa/inet.h>	//inet_addr
+#include <stdio.h>	//printf
+#include <sys/socket.h>	//socket
+#include <arpa/inet.h>	//inet_addr
 #include <unistd.h>	//write, close
 
 #include <sys/select.h>	//select
 
-int main(int argc ,char *argv[])
+#define STDIN 0
+#define STDOUT 1
+
+int readdata(int fd, char *data, size_t datalen)
+{
+	int retval = read(fd, data, datalen);
+	if (retval < 0)
+	{
+		if (fd == STDIN)
+		{
+			perror("read from stdin");
+		}
+		else
+		{
+			perror("read from socket");
+		}
+	}
+	return retval;
+}
+
+int writedata(int fd, char *data, size_t datalen) {
+	int retval = write(fd, data, datalen);
+	if (retval < 0) {
+		if (fd == STDOUT)
+		{
+			perror("write to stdout");
+		}
+		else
+		{
+			perror("write to socket");
+		}
+	}
+	return retval;
+}
+
+int selectdata(int sock, fd_set *readfds, fd_set *writefds, int has_data_for_tcp, int has_data_for_stdout) {
+	FD_ZERO(readfds);
+	if (!has_data_for_tcp) FD_SET(STDIN, readfds);
+	if (!has_data_for_stdout) FD_SET(sock, readfds);
+	FD_ZERO(writefds);
+	if (has_data_for_tcp) FD_SET(sock, writefds);
+	if (has_data_for_stdout) FD_SET(STDOUT, writefds);
+
+	int retval = select(sock+1, readfds, writefds, NULL, NULL);
+	if (retval < 0)
+	{
+		perror("select()");
+	}
+	return retval;
+}
+
+int main(int argc, char **argv)
 {
 	int sock;
 	struct sockaddr_in server;
@@ -38,54 +86,41 @@ int main(int argc ,char *argv[])
 
 	fd_set readfds, writefds;
 	int len_data_for_tcp = 0, len_data_for_stdout = 0;
+	int terminating = 0;
 
 	//keep communicating with server
-	for (;;)
+	while (!terminating || en_data_for_stdout || len_data_for_tcp)
 	{
-		FD_ZERO(&readfds);
-		if (!len_data_for_tcp) FD_SET(0, &readfds);
-		if (!len_data_for_stdout) FD_SET(sock, &readfds);
-		FD_ZERO(&writefds);
-		if (len_data_for_tcp) FD_SET(sock, &writefds);
-		if (len_data_for_stdout) FD_SET(1, &writefds);
-
-		if (-1 == select(sock+1, &readfds, &writefds, NULL, NULL)) {
-			perror("select()");
+		if (-1 == selectdata(sock, &readfds, &writefds, len_data_for_tcp, len_data_for_stdout))
+		{
 			return 1;
 		}
 
-		if (FD_ISSET(0, &readfds)) {
-			//write(1, "reading\n", 8);
-			len_data_for_tcp = read(0, message, sizeof(message));
+		if (FD_ISSET(STDIN, &readfds))
+		{
+			len_data_for_tcp = readdata(STDIN, message, sizeof(message));
 			if (len_data_for_tcp == 0) {
-				break;
+				terminating = 1;
 			}
 		}
 
-		//Receive a reply from the server
-		else if (FD_ISSET(sock, &readfds)) {
-			len_data_for_stdout = recv(sock, server_reply, sizeof(server_reply), 0);
-			if (len_data_for_stdout < 0)
-			{
-				fputs("recv failed\n", stderr);
-				break;
-			}
+		if (FD_ISSET(sock, &readfds))
+		{
+			len_data_for_stdout = readdata(sock, server_reply, sizeof(server_reply));
 			if (len_data_for_stdout == 0) {
-				break;
+				terminating = 1;
 			}
 		}
 
-		else if (FD_ISSET(1, &writefds)) {
-			write(1, server_reply, len_data_for_stdout);
+		if (FD_ISSET(STDOUT, &writefds))
+		{
+			writedata(STDOUT, server_reply, len_data_for_stdout);
 			len_data_for_stdout = 0;
 		}
 
-		else if (FD_ISSET(sock, &writefds)) {
-			if (send(sock, message, len_data_for_tcp, 0) < 0)
-			{
-				fputs("Send failed\n", stderr);
-				return 1;
-			}
+		if (FD_ISSET(sock, &writefds))
+		{
+			writedata(sock, message, len_data_for_tcp);
 			len_data_for_tcp = 0;
 		}
 	}
