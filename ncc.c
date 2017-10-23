195850080537021f66c49cb7fb4c16d834d2c896
Jørn Åne <git@jornane.no>
2017-10-23 23:11:56 +0200

    Select for both read and write

--- ncc.c
+++ ncc.c
@@ -2,7 +2,6 @@
 	C ECHO client example using sockets
 */
 #include<stdio.h>	//printf
-#include<string.h>	//strlen
 #include<sys/socket.h>	//socket
 #include<arpa/inet.h>	//inet_addr
 #include <unistd.h>	//write, close
@@ -13,7 +12,7 @@ int main(int argc ,char *argv[])
 {
 	int sock;
 	struct sockaddr_in server;
-	char message[1000], server_reply[2000];
+	char message[65535], server_reply[65535];
 
 	//Create socket
 	sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,46 +36,57 @@ int main(int argc ,char *argv[])
 
 	fputs("Connected\n", stderr);
 
-	fd_set readfds;
-	int retval;
-	int recvval;
+	fd_set readfds, writefds;
+	int len_data_for_tcp = 0, len_data_for_stdout = 0;
 
 	//keep communicating with server
 	for (;;)
 	{
 		FD_ZERO(&readfds);
-		FD_SET(0, &readfds);
-		FD_SET(sock, &readfds);
+		if (!len_data_for_tcp) FD_SET(0, &readfds);
+		if (!len_data_for_stdout) FD_SET(sock, &readfds);
+		FD_ZERO(&writefds);
+		if (len_data_for_tcp) FD_SET(sock, &writefds);
+		if (len_data_for_stdout) FD_SET(1, &writefds);
 
-		retval = select(sock+1, &readfds, NULL, NULL, NULL);
-		if (retval == -1) {
+		if (-1 == select(sock+1, &readfds, &writefds, NULL, NULL)) {
 			perror("select()");
 			return 1;
 		}
 
 		if (FD_ISSET(0, &readfds)) {
 			//write(1, "reading\n", 8);
-			read(0, message, sizeof(message));
-			//Send some data
-			if (send(sock, message, strlen(message) , 0) < 0)
-			{
-				fputs("Send failed\n", stderr);
-				return 1;
+			len_data_for_tcp = read(0, message, sizeof(message));
+			if (len_data_for_tcp == 0) {
+				break;
 			}
 		}
 
 		//Receive a reply from the server
-		if (FD_ISSET(sock, &readfds)) {
-			recvval = recv(sock, server_reply, sizeof(server_reply), 0);
-			if (recvval < 0)
+		else if (FD_ISSET(sock, &readfds)) {
+			len_data_for_stdout = recv(sock, server_reply, sizeof(server_reply), 0);
+			if (len_data_for_stdout < 0)
 			{
 				fputs("recv failed\n", stderr);
 				break;
 			}
-			if (recvval == 0) {
+			if (len_data_for_stdout == 0) {
 				break;
 			}
-			write(1, server_reply, strnlen(server_reply, sizeof(server_reply)));
+		}
+
+		else if (FD_ISSET(1, &writefds)) {
+			write(1, server_reply, len_data_for_stdout);
+			len_data_for_stdout = 0;
+		}
+
+		else if (FD_ISSET(sock, &writefds)) {
+			if (send(sock, message, len_data_for_tcp, 0) < 0)
+			{
+				fputs("Send failed\n", stderr);
+				return 1;
+			}
+			len_data_for_tcp = 0;
 		}
 	}
 
